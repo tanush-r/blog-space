b43f4738c19e1978359354a54ae66e99d154ac33
tanushmal <tanushmal@gmail.com>
2020-09-17 14:50:27 +0530

    new update

--- app.py
+++ app.py
@@ -2,17 +2,31 @@ from flask import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/compose')
 def compose():
     return render_template('compose.html')
 
+
 @app.route('/popular')
 def popular():
     return render_template('popular.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
